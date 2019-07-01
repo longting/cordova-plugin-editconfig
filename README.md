f2811f1b040f2e707b6ac1b178e644c427d3bfee
longting <longtinghappy@163.com>
2019-07-01 20:37:43 +0800

    说明文档

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cordova-plugin-editconfig
-解决Android9.0 http网络请求失败问题
+解决Android9.0 http网络请求失败问题  
+安装后无需编写任何代码。
+
+### 安装
+`cordova plugin add https://github.com/longting/cordova-plugin-editconfig.git`
--- scripts/beforePluginUninstall.js
+++ scripts/beforePluginUninstall.js
@@ -14,9 +14,6 @@ module.exports = function(context) {
   // android manifest file
   let androidManifestFile = path.join(platformAndroidAppModuleDir, 'src/main/AndroidManifest.xml');
 
-  // android app module gradle
-  let androidAppModuleGradleFile = path.join(platformAndroidAppModuleDir, 'build.gradle');
-
   if (fs.existsSync(androidManifestFile)) {
 
     fs.readFile(androidManifestFile, 'UTF-8', function(err, data) {
@@ -38,24 +35,5 @@ module.exports = function(context) {
       }
     });
   }
-  // if (fs.existsSync(androidAppModuleGradleFile)) {
 
-  //   fs.readFile(androidAppModuleGradleFile, 'UTF-8', function(err, data) {
-  //     if (err) {
-  //       throw new Error('Unable to find build.gradle: ' + err);
-  //     }
-
-  //     let androidAppModuleGradleConfigLine = 'apply from: "../com-jackro-example/app-jackroexample.gradle"';
-
-  //     if (data.indexOf(androidAppModuleGradleConfigLine) !== -1) {
-
-  //       let result = data.replace(androidAppModuleGradleConfigLine, '');
-
-  //       fs.writeFile(androidAppModuleGradleFile, result, 'UTF-8', function(err) {
-  //         if (err)
-  //           throw new Error('Unable to write into build.gradle: ' + err);
-  //       })
-  //     }
-  //   });
-  // }
 };
